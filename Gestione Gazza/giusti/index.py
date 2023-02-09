6e389d6517490b8daafa72fa9887c2e5c51e6674
noceg43 <noceg43@gmail.com>
2023-02-09 17:27:57 +0100

    index tmdb

--- Gestione Gazza/giusti/index.py
+++ Gestione Gazza/giusti/index.py
@@ -20,7 +20,37 @@ writer.commit()
 
 searcher = ix.searcher()
 
-query = QueryParser("content", ix.schema).parse("hat")
+cercato = "spider man"
+
+query = QueryParser("content", ix.schema).parse(cercato)
 results = searcher.search(query)
+print("                   Cercato           ", cercato)
+print("WIKIPEDIA")
+for result in results:
+    print(result['title'])
+
+
+
+schema = Schema(title=ID(stored=True), content=TEXT)
+
+
+ix = create_in("index_TMDB", schema)
+
+writer = ix.writer()
+
+with open('tmdbformattato.json', 'r') as f:
+    # Carica il contenuto del file in una lista
+    element_list = json.load(f)
+
+for element in element_list:
+    writer.add_document(title=element['title'], content=element['overview'])
+
+writer.commit()
+
+searcher = ix.searcher()
+
+query = QueryParser("content", ix.schema).parse(cercato)
+results = searcher.search(query)
+print("TMDB")
 for result in results:
     print(result['title'])
