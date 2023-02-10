f6afe5b1ccecca4abdf2a3fc84f823b1b9b9b324
noceg43 <noceg43@gmail.com>
2023-02-10 17:34:57 +0100

    generi

--- Gestione Gazza/giusti/index.py
+++ Gestione Gazza/giusti/index.py
@@ -2,15 +2,23 @@
 import argparse
 import json
 # Importo le librerie necessarie per creare gli indici Whoosh
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.fields import Schema, TEXT, ID, KEYWORD, STORED
 from whoosh.index import *
 from whoosh.qparser import *
+import time
+from whoosh.query import And, Regex, FuzzyTerm
+
+
+# Inizio a contare il tempo
+start = time.perf_counter()
 
 # Inizializzo l'oggetto argparse per gestire gli argomenti da riga di comando
 parser = argparse.ArgumentParser()
 # Aggiungo l'argomento "fonte", che può avere solo i valori "wikipedia" o "tmdb"
 parser.add_argument('-f', '--fonte', choices=['wikipedia', 'tmdb'],
                     help='Seleziona la fonte da cui cercare i risultati')
+parser.add_argument("-g", "--genere", nargs='+', help="Specificare i generi da cercare")
+
 # Aggiungo l'argomento "query", che è composto da una lista di parole da cercare
 parser.add_argument('query', nargs='+', help='Inserisci le parole da cercare')
 # Analizzo gli argomenti passati da riga di comando
@@ -18,9 +26,11 @@ args = parser.parse_args()
 
 # Unisco la lista di parole della query in una stringa separata da spazi
 query = ' '.join(args.query)
+generi = ' '.join(args.genere)
+
 
 # Creo lo schema per gli indici Whoosh, composto da un campo per il titolo (ID) e uno per il contenuto (TEXT)
-schema = Schema(title=ID(stored=True), content=TEXT)
+schema = Schema(title=ID(stored=True), content=TEXT, genres=KEYWORD)
 
 # Creo l'indice per wikipedia
 ix_wikipedia = create_in("index_wikipedia", schema)
@@ -36,7 +46,7 @@ with open('wikipediaformattato.json', 'r') as f:
 # Aggiungo i documenti all'indice
 for element in element_list:
     writer_wikipedia.add_document(
-        title=element['Title'], content=element['Plot'])
+        title=element['Title'], content=element['Plot'], genres = element['Genre'])
 
 # Commit delle modifiche all'indice
 writer_wikipedia.commit()
@@ -45,7 +55,8 @@ writer_wikipedia.commit()
 searcher_wikipedia = ix_wikipedia.searcher()
 
 # Ripeto lo stesso procedimento per l'indice di TMDB
-schema = Schema(title=ID(stored=True), content=TEXT)
+schema = Schema(title=ID(stored=True), content=TEXT, genres=KEYWORD)
+
 
 
 ix_TMDB = create_in("index_TMDB", schema)
@@ -56,9 +67,36 @@ with open('tmdbformattato.json', 'r') as f:
     # Carico il contenuto del file in una lista
     element_list = json.load(f)
 
+dizionario_id_TMDB = {
+    28: "action",
+    12: "adventure",
+    16: "animation",
+    35: "comedy",
+    80: "crime",
+    99: "documentary",
+    18: "drama",
+    10751: "family",
+    14: "fantasy",
+    36: "history",
+    27: "horror",
+    10402: "musical",
+    9648: "mystery",
+    10749: "romance",
+    878: "sci-fi",
+    10770: "TV Movie",
+    53: "thriller",
+    10752: "war",
+    37: "western"
+}
+
+
 for element in element_list:
     writer_TMDB.add_document(
-        title=element['title'], content=element['overview'])
+        title=element['title'], content=element['overview'], genres=" ".join([dizionario_id_TMDB[id] for id in element['genre_ids']]))
+    
+
+
+
 
 writer_TMDB.commit()
 
@@ -92,34 +130,53 @@ class Film:
         return self.title + f'{self.combined_score():.2f}'
 
 
-def search(query, source=None):
+def search(query, source=None, generi=None):
     # Inizializzare una lista vuota per i risultati
     films = []
     results_wikipedia = None
     results_tmdb = None
     # Cercare i risultati su Wikipedia se la fonte è specificata come 'wikipedia'
     if source == 'wikipedia':
-        results_wikipedia = searcher_wikipedia.search(
-            QueryParser("content", ix_wikipedia.schema).parse(query))
+        content_query = QueryParser("content", ix_wikipedia.schema).parse(query)
+
+        if generi is None:
+            results_wikipedia = searcher_wikipedia.search(content_query)
+        else:
+            genres_list = generi.split()
+            genres_query = [Regex("genres", ".*" + genre + ".*") for genre in genres_list]            
+            results_wikipedia = searcher_wikipedia.search(And([content_query, *genres_query]))
         print("Risultati da Wikipedia")
         for result in results_wikipedia:
             films.append(Film(result['title'], result.score, "wikipedia"))
     # Cercare i risultati su TMDB se la fonte è specificata come 'tmdb'
     elif source == 'tmdb':
-        results_tmdb = searcher_TMDB.search(
-            QueryParser("content", ix_TMDB.schema).parse(query))
+        content_query = QueryParser("content", ix_TMDB.schema).parse(query)
+        if generi is None:
+            results_tmdb = searcher_TMDB.search(content_query)
+        else:
+            genres_list = generi.split()
+            genres_query = [Regex("genres", ".*" + genre + ".*") for genre in genres_list]            
+            results_tmdb = searcher_TMDB.search(And([content_query, *genres_query]))            
         print("Risultati da TMDB")
         for result in results_tmdb:
             films.append(Film(result['title'], result.score, "tmdb"))
     # Cercare i risultati sia su Wikipedia che su TMDB se la fonte non è specificata
     elif source is None:
-        results_wikipedia = searcher_wikipedia.search(
-            QueryParser("content", ix_wikipedia.schema).parse(query))
+        content_query_wikipedia = QueryParser("content", ix_wikipedia.schema).parse(query)
+        content_query_tmdb = QueryParser("content", ix_TMDB.schema).parse(query)
+
+        if generi is None:
+            results_wikipedia = searcher_wikipedia.search(content_query_wikipedia)
+            results_tmdb = searcher_TMDB.search(content_query_tmdb)
+        else:
+            genres_list = generi.split()
+            genres_query = [Regex("genres", ".*" + genre + ".*") for genre in genres_list]            
+            results_wikipedia = searcher_wikipedia.search(And([content_query_wikipedia, *genres_query]))
+            results_tmdb = searcher_TMDB.search(And([content_query_tmdb, *genres_query]))            
+
         print("Risultati combinati:")
         for result in results_wikipedia:
             films.append(Film(result['title'], result.score, "wikipedia"))
-        results_tmdb = searcher_TMDB.search(
-            QueryParser("content", ix_TMDB.schema).parse(query))
         for result in results_tmdb:
             films.append(Film(result['title'], result.score, "tmdb"))
     # Stampare un messaggio di errore se la fonte non è valida e restituire
@@ -127,6 +184,7 @@ def search(query, source=None):
         print("Fonte non valida")
         return
     # Creare una lista di istanze Film per tutti i film trovati in entrambe le fonti
+    
     films = []
     if results_wikipedia is not None:
         for result in results_wikipedia:
@@ -135,6 +193,7 @@ def search(query, source=None):
     if results_tmdb is not None:
         for result in results_tmdb:
             films.append(Film(result['title'], 0, result.score))
+
 
     # Unire i film con lo stesso titolo
     films_combined = {}
@@ -154,10 +213,23 @@ def search(query, source=None):
         print(film.title, f'{film.combined_score():.2f}')
 
 
-search(query, args.fonte)
+search(query, args.fonte, generi)
 
 
 # query aggiungere tipo "solo film genere" o "solo film rating > x"
 
 # unire fonti dandogli peso
 # possibilità usare solo una fonte
+
+
+
+# Fine del conteggio del tempo
+end = time.perf_counter()
+
+# Stampa il tempo impiegato
+print("Tempo impiegato:", end - start, "secondi")
+
+
+
+
+
